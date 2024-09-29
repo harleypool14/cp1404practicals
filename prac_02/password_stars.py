8d569254e2bcbc49edb37d716891a618ddb5ab41
harle <harley.pool@my.jcu.edu.au>
2024-09-29 12:00:54 +1000

    Add password check program

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,20 +1,6 @@
 MIN_LENGTH = 6
-
-
-def main():
-    password = get_password()
-    asterisks(password)
-
-
-def asterisks(password):
-    print('*' * len(password))
-
-
-def get_password():
+password = input("Enter the password: ")
+while len(password) < MIN_LENGTH:
     password = input("Enter the password: ")
-    while len(password) < MIN_LENGTH:
-        password = input("Enter the password: ")
-    return password
+print('*' * len(password))
 
-
-main()
