131588833dc1760236d678959a9f4b66b63ea484
harle <harley.pool@my.jcu.edu.au>
2024-10-05 20:30:43 +1000

    Add password stars program

---
+++ prac_02/password_stars.py
@@ -0,0 +1,20 @@
+MIN_LENGTH = 6
+
+
+def main():
+    password = get_password()
+    asterisks(password)
+
+
+def asterisks(password):
+    print('*' * len(password))
+
+
+def get_password():
+    password = input("Enter the password: ")
+    while len(password) < MIN_LENGTH:
+        password = input("Enter the password: ")
+    return password
+
+
+main()
