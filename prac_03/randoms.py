976ae1176b85f3b1ce1d280746536f4c6ce90974
harle <harley.pool@my.jcu.edu.au>
2024-10-05 20:25:36 +1000

    Add randoms program

---
+++ prac_03/randoms.py
@@ -0,0 +1,30 @@
+"""
+what did you see on line 1?
+I saw 10, 18 and 19.
+What was the smallest number you could have seen, what was the largest?
+Smallest = 5
+Largest = 20
+
+What did you see on line 2?
+I saw 7, 9 and 7.
+What was the smallest number you could have seen, what was the largest?
+Smallest = 3
+Largest = 9
+Could line 2 have produced a 4?
+Line 2 can not produce 4 as the numbers can only go up by 2.
+
+What did you see on line 3?
+I saw 5.015015211767011, 4.724326232776185 and 2.64149067336749.
+What was the smallest number you could have seen, what was the largest?
+Smallest = 2.5
+Largest = 5.5
+"""
+
+# Write code, not a comment, to produce a random number between 1 and 100 inclusive
+import random
+
+print(random.randint(1, 100))
+
+
+
+
